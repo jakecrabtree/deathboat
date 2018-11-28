91661386770e3da3c0224d0623c84752cfd1c9c1
Jake <jakercrabtree@utexas.edu>
2018-11-28 12:03:56 -0600

    example scene with working triggers

---
+++ Assets/Scripts/TriggerModifier.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerModifier : MonoBehaviour {
+	
+	[SerializeField]
+	private string triggerName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other){
+        if (Input.GetKeyDown(KeyCode.E)){
+            if (other.CompareTag("Player")){
+				TriggerManager.UpdateTrigger(triggerName, true);
+            }
+        }
+    } 
+}
