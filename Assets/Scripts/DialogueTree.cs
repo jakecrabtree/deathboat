f157e7079f08f6d390c5d0aa614740a9f75c2126
Jake <jakercrabtree@utexas.edu>
2018-11-27 23:50:44 -0600

    persistant changes and overhaul

--- Assets/Scripts/DialogueTree.cs
+++ Assets/Scripts/DialogueTree.cs
@@ -2,23 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine; 
 
-public class DialogueEdgeList{
-    public HashSet<DialogueNode> nodes;
-    public HashSet<KeyValuePair<int,int>> edges;
 
-    public DialogueEdgeList(){
-        nodes = new HashSet<DialogueNode>();
-        edges = new HashSet<KeyValuePair<int,int>>();
+[System.Serializable]
+public class Edges{
+    public List<int> edges;
+    public Edges(){
+        edges = new List<int>();
     }
 }
 
-public class DialogueNode {
-    List<DialogueNode> children = new List<DialogueNode>();
+public class DialogueEdgeList{
 
+    public List<DialogueNode> nodes;
+
+    public HashSet<KeyValuePair<int,int>> edges;
+    
+    public DialogueEdgeList(){
+        nodes = new List<DialogueNode>();
+        edges = new HashSet<KeyValuePair<int,int>>();
+    }
+    public DialogueEdgeList(List<DialogueNode> nodes, HashSet<KeyValuePair<int,int>> edges){
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+}
+[System.Serializable]
+public class DialogueNode {
+    [SerializeField]
     string text;
+    [SerializeField]
     string trigger;
+    [SerializeField]
     int priority;
+    [SerializeField]
     int id;
+    [SerializeField]
     Rect editorBox;
     public DialogueNode(string text, int id = -1, string trigger = null, int priority = 0){
         this.text = text;
@@ -35,49 +53,6 @@ public class DialogueNode {
         editorBox = window.window;
     }
 
-    private DialogueNode selectChild(){
-        foreach(DialogueNode child in children){
-            if(child.trigger != null && DialogueManager.GetTrigger(child.trigger)){
-                return child;
-            }else if (child.trigger == null){
-                return child;
-            }
-        }
-        return null;
-    }
-
-    public DialogueNode next(){
-        return (empty()) ? null : selectChild();
-    }
-
-    public DialogueEdgeList BFSTraverse(){
-        DialogueEdgeList ret = new DialogueEdgeList();
-        Queue<DialogueNode> nodeQueue = new Queue<DialogueNode>();
-
-        nodeQueue.Enqueue(this);
-        DialogueNode curr;
-        while (nodeQueue.Count > 0){
-            curr = nodeQueue.Dequeue();
-            ret.nodes.Add(curr);
-            foreach(DialogueNode node in curr.children){
-                if (!ret.nodes.Contains(node)){
-                    nodeQueue.Enqueue(node);
-                }
-                ret.edges.Add(new KeyValuePair<int, int>(curr.id, node.id));
-            }
-        }
-        return ret;
-    }
-
-    public void addChild(DialogueNode node){
-        children.Add(node);
-        children.Sort((x,y)=> x.priority.CompareTo(y.priority));
-    }
-
-    public bool empty(){
-        return children.Count == 0;
-    }
-
     public NodeWindow toNodeWindow(){
         if (id == 0){
             editorBox = new Rect(NodeWindow.DEFAULT_WINDOW_X_POS, NodeWindow.DEFAULT_WINDOW_Y_POS, 
@@ -87,50 +62,66 @@ public class DialogueNode {
     }
 }
 
-
-
-[ExecuteInEditMode]
 public class DialogueTree : MonoBehaviour{
-    DialogueNode root = new DialogueNode("root", 0); 
-    DialogueNode curr;
-
     
-    public void Awake(){
-        this.curr = root; 
-    }
+    [SerializeField]
+    private List<DialogueNode> nodes = new List<DialogueNode>();
+
+    [SerializeField]
+    private List<Edges> adjList = new List<Edges>();
+
+    private int root = 0;
+    private int curr = 0;
 
     public DialogueNode current(){
-        return curr;
+        return nodes[curr];
     }
 
     public DialogueNode next(){
         if (!isEnd()){
-            curr = curr.next();
+            curr = selectChild();
         }
-        return curr;
+        return current();
+    }
+
+    private int selectChild(){
+        return adjList[curr].edges[0]; 
     }
 
     public bool isEnd(){
-        return curr.empty();
+        return (adjList.Count >= curr) && adjList[curr].edges.Count > 0;
     }
 
     public void reset(){
         curr = root;
     }
 
-    public void addChild(DialogueNode node){
-        if (curr != null){
-            curr.addChild(node);
-        }   
+    public void AddNode(DialogueNode node){
+        nodes.Add(node);
     }
 
     public DialogueEdgeList toEdgeList(){
-        return root.BFSTraverse();
+        DialogueEdgeList list = new DialogueEdgeList();
+        list.nodes = nodes;
+        int count = 0;
+        foreach(Edges edges in adjList){
+            foreach(int index in edges.edges){
+                list.edges.Add(new KeyValuePair<int, int>(count, index));
+            }
+            ++count;
+        }
+        return list;
     }
 
-    public void setRoot(DialogueNode root){
-        this.root = root;
-        reset();
+    public void fromEdgeList(DialogueEdgeList edgeList){
+        this.nodes = edgeList.nodes;
+        adjList = new List<Edges>();
+        foreach (DialogueNode node in nodes){
+            adjList.Add(new Edges());
+        }
+        foreach(KeyValuePair<int,int> edge in edgeList.edges){
+            this.adjList[edge.Key].edges.Add(edge.Value);
+        } 
     }
 
 }
