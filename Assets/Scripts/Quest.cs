701445cd12b1d077952c6ed09bbbaf31a4eabcd9
Jake <jakercrabtree@utexas.edu>
2018-12-08 12:56:12 -0600

    initial quest structure

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Quest : MonoBehaviour {
+
+	[SerializeField]
+	private QuestItem[] items;
+
+	// Use this for initialization, MUST CALL base.Start();
+	void Start () {
+		foreach(QuestItem item in items){
+			if (item != null){
+				item.AssignQuest(this);
+			}
+			else{
+				Debug.Log("Quest cannot contain null items");
+			}
+		}
+	}
+
+	
+
+	public abstract bool CollectItem(QuestItem item);
+}
---
+++ Assets/Scripts/QuestItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestItem : MonoBehaviour {
+
+	Quest quest;
+	
+	[SerializeField]
+	string itemName = "";
+
+	[SerializeField]
+	string triggerName;
+	bool collected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other){
+        if (Input.GetKeyDown(KeyCode.E)){
+            if (other.CompareTag("Player") && !collected){
+				TriggerManager.UpdateTrigger(triggerName, true);
+            }
+        }
+    }
+
+	public void AssignQuest(Quest quest){
+		this.quest = quest;
+	}
+}
