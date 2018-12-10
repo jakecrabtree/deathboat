601e37137a035cdeb58953bc125720a4c3c9db63
Garrett Bishop <35820287+gbishop888@users.noreply.github.com>
2018-12-10 01:50:12 -0600

    Added Scripts
    Added Audio and boombox scripts

---
+++ Assets/Scripts/BoomBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (other.CompareTag("Player"))
+            {
+                GameObject.Find("SoundManager").GetComponent<SoundManager>().playEyeOfTheTiger();
+            }
+        }
+	}
+}
