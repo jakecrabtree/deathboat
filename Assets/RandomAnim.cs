6dc750f53c227d17fec9382e9d52371ef7d4a292
kevin <kevin.lam72003@gmail.com>
2018-12-09 23:45:42 -0600

    Add slots flashing

---
+++ Assets/RandomAnim.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// starts animation clip "Anim" at a random position
+public class RandomAnim : MonoBehaviour {
+
+	void Start () {
+		GetComponent<Animator>().Play("Anim", -1, Random.Range(0f, 1f));
+	}
+}
